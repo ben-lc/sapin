5f2b13d6546454d434e2bf9ab4fb11b10741d016
ben-lc <38050807+ben-lc@users.noreply.github.com>
2023-04-24 22:54:12 +0200

    init commit

---
+++ src/test/kotlin/fr/sacoche/sapin/SapinApplicationTests.kt
@@ -0,0 +1,13 @@
+package fr.sacoche.sapin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SapinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
