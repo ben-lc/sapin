1fcff6dea9992f1167eb44af4e2f83398e585bcd
ben-lc <38050807+ben-lc@users.noreply.github.com>
2023-05-07 12:50:43 +0200

    feat(etl): setup scriptella etl ant task to load data and flyway gradle plugin to handle schema updates from gradle

---
+++ etl/build.gradle.kts
@@ -0,0 +1,47 @@
+plugins {
+  id("org.flywaydb.flyway") version "9.17.0"
+  id("sacoche.common-conventions")
+  id("sacoche.sql-conventions")
+}
+
+extra["scriptellaVersion"] = "1.2"
+
+extra["postgresDriverVersion"] = "42.5.0"
+
+extra["scriptellaVersion"] = "1.2"
+
+val scriptella by configurations.creating
+
+dependencies {
+  scriptella("org.scriptella:scriptella-core:${property("scriptellaVersion")}")
+  scriptella("org.scriptella:scriptella-tools:${property("scriptellaVersion")}")
+  scriptella("org.scriptella:scriptella-drivers:${property("scriptellaVersion")}")
+  scriptella("org.postgresql:postgresql:${property("postgresDriverVersion")}")
+}
+
+tasks.register("loadDatabase") {
+  ant.setProperty("csv.location", "../input")
+  ant.setProperty("db.url", "jdbc:postgresql://localhost/db_sapin")
+  ant.setProperty("db.user", "postgres")
+  ant.setProperty("db.password", "postgres")
+  doLast {
+    ant.withGroovyBuilder {
+      "taskdef"(
+          "resource" to "antscriptella.properties",
+          "classpath" to configurations["scriptella"].asPath)
+      "etl"("nostat" to true, "nojmx" to true) {
+        "fileset"("dir" to "src/main/resources/etl", "includes" to "*.etl.xml")
+      }
+    }
+  }
+}
+
+flyway {
+  url = "jdbc:postgresql://localhost/db_sapin"
+  user = "postgres"
+  password = "postgres"
+  schemas = arrayOf("sapin")
+  locations = arrayOf("filesystem:../src/main/resources/db/migration")
+  configurations = arrayOf("scriptella")
+  cleanDisabled = false
+}
