3adece9d889f94930013c75c1159da04f2e563cc
ben-lc <38050807+ben-lc@users.noreply.github.com>
2023-06-16 17:08:26 +0200

    feat: add dgs graphql data fetcher for location

---
+++ graphql/src/main/kotlin/com/github/ben_lc/sapin/graphql/type/EntityExtensions.kt
@@ -0,0 +1,13 @@
+package com.github.ben_lc.sapin.graphql.type
+
+import com.github.ben_lc.sapin.generated.graphql.types.Location
+import com.github.ben_lc.sapin.model.LocationEntity
+
+fun LocationEntity.toGraphqlType(): Location =
+    Location(
+        id = this.id.toString(),
+        name = this.name,
+        level = this.level,
+        levelLocalName = this.levelLocalName,
+        levelLocalNameEn = this.levelLocalNameEn,
+        isoId = this.isoId)
