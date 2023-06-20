bd7d2afe06264a9e534233eb4d0e94a375fda85c
ben-lc <38050807+ben-lc@users.noreply.github.com>
2023-06-20 16:31:19 +0200

    feat: add graphql data fetchers for children and parent of location

--- graphql/src/test/kotlin/com/github/ben_lc/sapin/graphql/datafetcher/LocationControllerTests.kt
+++ graphql/src/test/kotlin/com/github/ben_lc/sapin/graphql/datafetcher/LocationControllerTests.kt
@@ -1,7 +1,7 @@
 package com.github.ben_lc.sapin.graphql.datafetcher
 
-import com.github.ben_lc.sapin.graphql.type.LocationType
-import com.github.ben_lc.sapin.model.Location
+import com.github.ben_lc.sapin.graphql.type.Location
+import com.github.ben_lc.sapin.model.LocationEntity
 import com.github.ben_lc.sapin.repository.LocationRepository
 import com.ninjasquad.springmockk.MockkBean
 import io.mockk.coEvery
@@ -19,7 +19,7 @@ class LocationControllerTests {
 
   @Test
   fun `locationById should return location matching given id`() {
-    coEvery { locationRepo.findById(3) } returns Location(id = 3, name = "France", level = 1)
+    coEvery { locationRepo.findById(3) } returns LocationEntity(id = 3, name = "France", level = 1)
 
     tester
         .document(
@@ -27,8 +27,8 @@ class LocationControllerTests {
         )
         .execute()
         .path("data.locationById")
-        .entity(LocationType::class.java)
-        .isEqualTo(LocationType(id = "3", name = "France", level = 0))
+        .entity(Location::class.java)
+        .isEqualTo(Location(id = "3", name = "France", level = 0))
   }
 
   @Test
@@ -45,41 +45,83 @@ class LocationControllerTests {
   fun `locationBySimilarName should return list of location similar to given name`() {
     coEvery { locationRepo.findAllBySimilarName("trance", 1, 10) } returns
         flowOf(
-            Location(id = 3, name = "France", level = 1),
-            Location(id = 4, name = "Greece", level = 1))
+            LocationEntity(id = 3, name = "France", level = 1),
+            LocationEntity(id = 4, name = "Greece", level = 1))
     tester
         .document("""{ locationsBySimilarName(name: "trance", level: 1) { id name level } }""")
         .execute()
         .path("data.locationsBySimilarName[*]")
-        .entityList(LocationType::class.java)
+        .entityList(Location::class.java)
         .containsExactly(
-            LocationType(id = "3", name = "France", level = 1),
-            LocationType(id = "4", name = "Greece", level = 1))
+            Location(id = "3", name = "France", level = 1),
+            Location(id = "4", name = "Greece", level = 1))
   }
 
   @Test
   fun `locationByGeolocation should return list of location containing given coordinates for given level`() {
     coEvery { locationRepo.findAllByGeolocationAndLevel(1.0, 1.0, 1) } returns
-        flowOf(Location(id = 3, name = "France", level = 1))
+        flowOf(LocationEntity(id = 3, name = "France", level = 1))
     tester
         .document(
             """{ locationsByGeolocation(longitude: 1.0 , latitude: 1.0, level: 1) { id name level } }""")
         .execute()
         .path("data.locationsByGeolocation[*]")
-        .entityList(LocationType::class.java)
-        .containsExactly(LocationType(id = "3", name = "France", level = 1))
+        .entityList(Location::class.java)
+        .containsExactly(Location(id = "3", name = "France", level = 1))
   }
 
   @Test
   fun `locationByGeolocation should return list of location containing given coordinates`() {
     coEvery { locationRepo.findAllByGeolocation(1.0, 1.0) } returns
-        flowOf(Location(id = 3, name = "France", level = 1))
+        flowOf(LocationEntity(id = 3, name = "France", level = 1))
     tester
         .document(
             """{ locationsByGeolocation(longitude: 1.0 , latitude: 1.0) { id name level } }""")
         .execute()
         .path("data.locationsByGeolocation[*]")
-        .entityList(LocationType::class.java)
-        .containsExactly(LocationType(id = "3", name = "France", level = 1))
+        .entityList(Location::class.java)
+        .containsExactly(Location(id = "3", name = "France", level = 1))
+  }
+
+  @Test
+  fun `children should returns batched list of children`() {
+    coEvery { locationRepo.findAllBySimilarName("fr", 1) } returns
+        flowOf(
+            LocationEntity(id = 3, name = "France", level = 1),
+            LocationEntity(id = 1, name = "Italy", level = 1))
+    coEvery { locationRepo.findChildrenByIdIn(listOf(3, 1)) } returns
+        flowOf(
+            LocationEntity(id = 7, name = "Bretagne", level = 2, parentId = 3),
+            LocationEntity(id = 8, name = "Bourgogne", level = 2, parentId = 3),
+            LocationEntity(id = 9, name = "Lazio", level = 2, parentId = 1),
+            LocationEntity(id = 10, name = "Toscana", level = 2, parentId = 1))
+
+    tester
+        .document(
+            """{ locationsBySimilarName(name: "fr", level: 1) { name children { id name } } }""")
+        .execute()
+        .path("data.locationsBySimilarName")
+        .matchesJsonStrictly(
+            """[{"name":"France","children":[{"id":"7","name":"Bretagne"},{"id":"8","name":"Bourgogne"}]},{"name":"Italy","children":[{"id":"9","name":"Lazio"},{"id":"10","name":"Toscana"}]}]""")
+  }
+
+  @Test
+  fun `paren should returns batched list of parent`() {
+    coEvery { locationRepo.findAllBySimilarName("fr", 1) } returns
+        flowOf(
+            LocationEntity(id = 7, name = "Bretagne", level = 2, parentId = 3),
+            LocationEntity(id = 9, name = "Lazio", level = 2, parentId = 1))
+    coEvery { locationRepo.findAllById(listOf(3, 1)) } returns
+        flowOf(
+            LocationEntity(id = 3, name = "France", level = 1),
+            LocationEntity(id = 1, name = "Italy", level = 1))
+
+    tester
+        .document(
+            """{ locationsBySimilarName(name: "fr", level: 1) { name parent { id name } } }""")
+        .execute()
+        .path("data.locationsBySimilarName")
+        .matchesJsonStrictly(
+            """[{"name":"Bretagne","parent":{"id":"3","name":"France"}},{"name":"Lazio","parent":{"id":"1","name":"Italy"}}]""")
   }
 }
