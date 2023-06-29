610a57da51e58856f7860b6566582ac485b03266
ben-lc <38050807+ben-lc@users.noreply.github.com>
2023-06-29 19:38:21 +0200

    build: add gradle axion release plugin to set project version from git tags

--- buildSrc/src/main/java/com/github/ben_lc/sapin/sapin.common-conventions.gradle.kts
+++ buildSrc/src/main/java/com/github/ben_lc/sapin/sapin.common-conventions.gradle.kts
@@ -1,8 +1,11 @@
 plugins {
   id("com.diffplug.spotless")
   id("com.github.ben-manes.versions")
+  id("pl.allegro.tech.build.axion-release")
 }
 
 spotless {
   kotlinGradle { ktfmt() }
 }
+
+version = scmVersion.version
