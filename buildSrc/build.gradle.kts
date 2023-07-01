a953536b294018c023c699569fe0987766e40608
ben-lc <38050807+ben-lc@users.noreply.github.com>
2023-07-01 16:21:47 +0200

    Ci/add docker image support (#10)
    * build: add gradle axion release plugin to set project version from git tags
    
    * build: configure docker image name generated by bootBuildImage task
    
    * ci: use temurin jvm to build project
    
    * ci: add git action to build docker image with spring native and push it to github packages
    
    * feat: add spring console banner, set application name and always enable graphiql

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -11,6 +11,7 @@ dependencies {
   implementation("io.spring.gradle:dependency-management-plugin:1.1.0")
   implementation("com.diffplug.spotless:spotless-plugin-gradle:6.18.0")
   implementation("com.github.ben-manes:gradle-versions-plugin:0.46.0")
+  implementation("pl.allegro.tech.build.axion-release:pl.allegro.tech.build.axion-release.gradle.plugin:1.15.3")
   implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.22")
   implementation("org.jetbrains.kotlin:kotlin-allopen:1.8.22")
 }
