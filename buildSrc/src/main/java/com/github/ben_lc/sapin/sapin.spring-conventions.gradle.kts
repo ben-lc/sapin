18feb7ab0904c5ccd736c2cd89a334d6bf93c86c
ben-lc <38050807+ben-lc@users.noreply.github.com>
2023-07-04 12:09:06 +0200

    style: apply yaml formatter

--- buildSrc/src/main/java/com/github/ben_lc/sapin/sapin.spring-conventions.gradle.kts
+++ buildSrc/src/main/java/com/github/ben_lc/sapin/sapin.spring-conventions.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
   id("io.spring.dependency-management")
+  id("com.diffplug.spotless")
   kotlin("plugin.spring")
 }
 
@@ -9,4 +10,11 @@ extra["springMockVersion"] = "4.0.2"
 dependencyManagement {
   imports { mavenBom(org.springframework.boot.gradle.plugin.SpringBootPlugin.BOM_COORDINATES) }
   imports { mavenBom("org.testcontainers:testcontainers-bom:${property("testcontainersVersion")}") }
+}
+
+spotless {
+  yaml {
+    target("src/**/*.yml")
+    jackson()
+  }
 }
