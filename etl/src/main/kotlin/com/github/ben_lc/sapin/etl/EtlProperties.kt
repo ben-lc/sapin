7574890b799cd9170a32b19b6e312fec1de452f1
ben-lc <38050807+ben-lc@users.noreply.github.com>
2023-07-04 13:46:23 +0200

    feat: set properties of location geopackage to load in spring configuration file

--- etl/src/main/kotlin/com/github/ben_lc/sapin/etl/EtlProperties.kt
+++ etl/src/main/kotlin/com/github/ben_lc/sapin/etl/EtlProperties.kt
@@ -1,15 +1,41 @@
 package com.github.ben_lc.sapin.etl
 
 import org.springframework.boot.context.properties.ConfigurationProperties
-import org.springframework.boot.context.properties.bind.ConstructorBinding
 import org.springframework.core.io.Resource
 
 @ConfigurationProperties(prefix = "etl", ignoreUnknownFields = false)
-data class EtlProperties @ConstructorBinding constructor(val scriptella: ScriptellaProperties)
+data class EtlProperties(val scriptella: ScriptellaProperties, val load: LoadProperties)
 
-data class ScriptellaProperties
-@ConstructorBinding
-constructor(val config: ScriptellaConfigProperties)
+data class ScriptellaProperties(val config: ScriptellaConfigProperties)
 
 /** @property location the location of scriptella xml configuration files */
-data class ScriptellaConfigProperties @ConstructorBinding constructor(val location: Resource)
+data class ScriptellaConfigProperties(val location: Resource)
+
+data class LoadProperties(val geopackage: GeopackageProperties)
+
+data class GeopackageProperties(val location: List<LocationGeopackageProperties>)
+
+/**
+ * Properties of the location geopackage to load.
+ *
+ * @property tableName the name of the geopackage table to load
+ * @property nameColumnName the table column name containing the name of locations
+ * @property level the level number of the locations
+ * @property srcIdColumnName the table column name containing unique id of source data
+ * @property isoIdColumnName the table column name containing iso id (eg: 3166-2) of the locations
+ * @property levelNameColumnName the table column name containing the level name in the local
+ *   language
+ * @property levelNameEnColumnName the table column name containing the level name in english
+ * @property srcParentIdColumnName the table column name containing parent unique id for sub
+ *   divisions
+ */
+data class LocationGeopackageProperties(
+    val tableName: String,
+    val nameColumnName: String,
+    val level: Int,
+    val srcIdColumnName: String,
+    val isoIdColumnName: String? = null,
+    val levelNameColumnName: String? = null,
+    val levelNameEnColumnName: String? = null,
+    val srcParentIdColumnName: String? = null
+)
